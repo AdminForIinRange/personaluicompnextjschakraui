e058a81ef9431ed39eefbca95f38aefbc772beea
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-03 17:23:26 +1030

    Nextjs and chakra UI

---
+++ src/app/layout.js
@@ -0,0 +1,25 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+import { ChakraProvider } from '@chakra-ui/react'
+
+export const metadata = {
+  title: {
+    default: "Next.js 14 Homepage",
+    template: "%s | Next.js 14",
+  },
+  description: "Next.js starter app description",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <ChakraProvider>
+          {children}
+        </ChakraProvider>
+      </body>
+    </html>
+  )
+}
