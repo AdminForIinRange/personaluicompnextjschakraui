5b6964189d5e1b4d477087df042ddc49874fa57b
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-06 11:16:05 +1030

    understanding [slug] even if it may be bad
    import NotFound from '@/app/not-found';
    import Card from '@/components/presets/Card';
    import React from 'react';
    
    const Single = ({ params }) => {
      if (params.slug === 'cards') {
        return (
          <div>
            <Card />
          </div>
        );
      } else if (params.slug === 'modal') {
        return (
          <div>
            <hi>Modal</hi>
          </div>
        );
      } else if (params.slug === 'frames') {
        return (
          <div>
            <hi>Frames</hi>
          </div>
        );
      } else if (params.slug === 'animations') {
        return (
          <div>
            <hi>Animations</hi>
          </div>
        );
      } else if (params.slug === 'charts') {
        return (
          <div>
            <hi>Charts</hi>
          </div>
        );
      } else if (params.slug === 'loginForm') {
        return (
          <div>
            <hi>Login Form</hi>
          </div>
        );
      } else if (params.slug === 'Hero') {
        return (
          <div>
            <hi>Hero</hi>
          </div>
        );
      } else {
        return <NotFound />;
      }
    };
    
    export default Single;

--- src/app/components/[slug]/page.jsx
+++ src/app/components/[slug]/page.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import NotFound from '@/app/not-found';
+import Card from '@/components/presets/Card';
+import React from 'react';
 
 const Single = ({ params }) => {
-  return (
-    
-    <div>
-      <h1>{params.slug}</h1>
-    </div>
-  )
-}
+  if (params.slug === 'cards') {
+    return (
+      <div>
+        <Card />
+      </div>
+    );
+  } else if (params.slug === 'modal') {
+    return (
+      <div>
+        <hi>Modal</hi>
+      </div>
+    );
+  } else if (params.slug === 'frames') {
+    return (
+      <div>
+        <hi>Frames</hi>
+      </div>
+    );
+  } else if (params.slug === 'animations') {
+    return (
+      <div>
+        <hi>Animations</hi>
+      </div>
+    );
+  } else if (params.slug === 'charts') {
+    return (
+      <div>
+        <hi>Charts</hi>
+      </div>
+    );
+  } else if (params.slug === 'loginForm') {
+    return (
+      <div>
+        <hi>Login Form</hi>
+      </div>
+    );
+  } else if (params.slug === 'Hero') {
+    return (
+      <div>
+        <hi>Hero</hi>
+      </div>
+    );
+  } else {
+    return <NotFound />;
+  }
+};
 
-export default Single
+export default Single;
