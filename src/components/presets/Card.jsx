794218195ca5bf7501951fc161a7a65ba06a2117
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-06 13:10:02 +1030

    odd icon formating
        <HStack>
                    <Text as={"span"}>Learn more</Text>{" "}
                    <HiArrowLongRight fontSize={"25px"} />
                  </HStack>

--- src/components/presets/Card.jsx
+++ src/components/presets/Card.jsx
@@ -32,42 +32,48 @@ const Card = () => {
         </VStack>
 
         <HStack mt={"20px"} justify={"left"} w={"100%"} h={"100%"}>
-        <Box
-                w={"290px"}
-                h={"310px"}
-                border={"1.8px solid #B5B5B5"}
-                rounded={"xl"}
-                transition="transform, 0.3s ease-in-out"
-                _hover={{
-                
-                  boxShadow: "0 5px 10px gray",
-                }}
-                p={"3"}
-              > 
-
-              <VStack justify={"left"} align={"inherit"} w={"100%"} h={"100%"}>
-        
-              
-              <HStack w={"50%"} >
-              <Text align={"center"} fontSize={"32px"} fontWeight={"700"} textAlign={"left"}  >
-                      The Dolomites
-                      </Text>
+          <Box
+            w={"290px"}
+            h={"310px"}
+            border={"1.8px solid #B5B5B5"}
+            rounded={"xl"}
+            transition="transform, 0.3s ease-in-out"
+            _hover={{
+              boxShadow: "0 5px 10px gray",
+            }}
+            p={"3"}
+          >
+            <VStack justify={"left"} align={"inherit"} w={"100%"} h={"100%"}>
+              <HStack w={"50%"}>
+                <Text
+                  align={"center"}
+                  fontSize={"32px"}
+                  fontWeight={"700"}
+                  textAlign={"left"}
+                >
+                  The Dolomites
+                </Text>
               </HStack>
 
-              <HStack w={"100%"} >
-              <Text align={"center"} fontSize={"16px"} fontWeight={"500"} textAlign={"left"} >
-              The Dolomites, also known as the Dolomite Mountains, Dolomite Alps or Dolomitic Alps, are a mountain range in northeastern Italy.
-                      </Text>
+              <HStack w={"100%"}>
+                <Text
+                  align={"center"}
+                  fontSize={"16px"}
+                  fontWeight={"500"}
+                  textAlign={"left"}
+                >
+                  The Dolomites, also known as the Dolomite Mountains, Dolomite
+                  Alps or Dolomitic Alps, are a mountain range in northeastern
+                  Italy.
+                </Text>
               </HStack>
 
               <HStack>
-
+                <Text as={"span"}>Learn more</Text>{" "}
+                <HiArrowLongRight fontSize={"25px"} />
               </HStack>
-
-                      
-              </VStack>
-               
-              </Box>
+            </VStack>
+          </Box>
         </HStack>
       </VStack>
     </>
