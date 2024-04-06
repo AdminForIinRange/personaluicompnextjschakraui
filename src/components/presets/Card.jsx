8e102920d81f8b45977bc8698ea5cbb5d89e25f7
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-06 16:41:27 +1030

    using Prisim to dysplay code

--- src/components/presets/Card.jsx
+++ src/components/presets/Card.jsx
@@ -8,6 +8,7 @@ import {
   MenuList,
   MenuItem,
   Spacer,
+  
   ButtonGroup,
   useMediaQuery,
   Modal,
@@ -31,6 +32,11 @@ import ImageSimpleCard from "./BasicCards/ImageSimpleCard";
 import CollageCard from "./BasicCards/collage/CollageCard";
 import HrzonCard from "./BasicCards/collage/HrzonCard";
 import TicketCard from "./ComplexCards/TicketCard";
+import 'prismjs/themes/prism.css'; // Import the default Prism CSS
+import 'prismjs'; // Import Prism.js
+import 'prismjs/components/prism-javascript';
+
+
 const Card = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
   return (
@@ -73,8 +79,10 @@ const Card = () => {
           </HStack>
 
         <HStack mt={"20px"} justify={"left"} w={"100%"} h={"100%"} gap={"15px"} flexWrap={"wrap"}  align={"left"}>
-<CollageCard />
-<HrzonCard />
+ <CollageCard />
+ <Box as={"div"} onClick={onOpen} valuePayload={"HrzonCard"}> <HrzonCard /></Box>
+
+
         </HStack>
 
         <HStack
@@ -94,6 +102,91 @@ const Card = () => {
 
         </HStack>
       </VStack>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Code</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          <pre>
+    <code className="language-javascript">
+      {`<Box
+  w={"650px"}
+  h={"310px"}
+  border={"1.8px solid #B5B5B5"}
+  rounded={"xl"}
+  p={"4"}
+>
+  <HStack w={"100%"} h={"100%"} align={"left"} px={2}>
+    <HStack justify={"left"} align={"start"} w={"100%"} h={"100%"}>
+      <HStack w={"100%"} h={"100%"}>
+        <Box
+          w={"100%"}
+          h={"100%"}
+          bgColor={"black"}
+          rounded={"xl"}
+          bgImage={
+            "https://source.unsplash.com/pathway-between-green-trees-F_hft1Wiyj8"
+          }
+          bgSize={"cover"}
+          bgPos={["center", " center", "center", "center", "center"]}
+        ></Box>
+      </HStack>
+      <HStack w={"200%"} h={"100%"}>
+        <Box
+          w={"100%"}
+          h={"100%"}
+          bgColor={"black"}
+          rounded={"xl"}
+          bgImage={
+            "https://source.unsplash.com/birds-eye-view-photograph-of-green-mountains-01_igFr7hd4"
+          }
+          bgSize={"cover"}
+          bgPos={["top ", " top ", "top", "top", "top"]}
+        ></Box>
+      </HStack>
+      <HStack w={"100%"} h={"100%"}>
+        <Box
+          w={"100%"}
+          h={"100%"}
+          bgColor={"black"}
+          rounded={"xl"}
+          bgImage={
+            "https://source.unsplash.com/aerial-photo-of-green-trees-ugnrXk1129g" //cache
+          }
+          bgSize={"cover"}
+          bgPos={["bottom right", " bottom right", "bottom right ", "bottom right ", "bottom right"]}
+        ></Box>
+      </HStack>
+      <HStack w={"50%"} h={"100%"}>
+        <Box
+          w={"100%"}
+          h={"100%"}
+          bgColor={"black"}
+          rounded={"xl"}
+          bgImage={
+            "https://source.unsplash.com/photo-of-green-plants-7APM4YyW_JU"
+          }
+          bgSize={"cover"}
+          bgPos={["top right", " top right", "top right", "top right", "top "]}
+        ></Box>
+      </HStack>
+    </HStack>
+  </HStack>
+</Box>`}
+    </code>
+  </pre>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
