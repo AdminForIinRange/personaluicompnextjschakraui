789fdddca28189fb3fc47df1bc6d72bb61b6f44b
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-07 18:11:09 +0930

    frmoting codebase

--- src/components/navbar/Links/Links.jsx
+++ src/components/navbar/Links/Links.jsx
@@ -23,56 +23,53 @@ export const Links = () => {
               width: "170px",
             }}
           >
-<Image src={fruitImage} alt="Fruit Image" width={20}  style={{transform: "rotate(25deg)", marginRight: "5px"}}  />
-
+            <Image
+              src={fruitImage}
+              alt="Fruit Image"
+              width={20}
+              style={{ transform: "rotate(25deg)", marginRight: "5px" }}
+            />
 
             <Text as={"span"} color={"white"}>
               {" "}
-                   Presets
+              Presets
             </Text>
           </Button>
-        </Link>
-       
-          {" "}
-          <Button
-            target="_blank"
-            href="https://www.linkedin.com/in/anjesh-bhattarai/"
-            leftIcon={<FaLinkedin color="white" size={"20px"} />}
-            variant={"solid"}
-            bgColor={"#63B3ED"}
-            w={"100px"}
-            transition="transform, 0.3s ease-in-out"
-            _hover={{
-              transform: "scale(1.03)",
-              boxShadow: "0 5px 25px gray",
-              width: "170px",
-            }}
-          >
-            <Text color={"white"} as={"span"}>
-              Linkden{" "}
-            </Text>
-          </Button>
-      
-       
-          {" "}
-          <Button
-          
-            href="https://github.com/AdminForIinRange"
-            target="_blank"
-            leftIcon={<FaGithub color="WHITE" size={"20px"} />}
-            bgColor={"gray"}
-            variant={"solid"}
-            w={"100px"}
-            transition="transform, 0.3s ease-in-out"
-            _hover={{
-              transform: "scale(1.03)",
-              boxShadow: "0 5px 25px gray",
-              width: "170px",
-            }}
-          >
-            <Text color={"white"}>Github</Text>
-          </Button>
-     
+        </Link>{" "}
+        <Button
+          target="_blank"
+          href="https://www.linkedin.com/in/anjesh-bhattarai/"
+          leftIcon={<FaLinkedin color="white" size={"20px"} />}
+          variant={"solid"}
+          bgColor={"#63B3ED"}
+          w={"100px"}
+          transition="transform, 0.3s ease-in-out"
+          _hover={{
+            transform: "scale(1.03)",
+            boxShadow: "0 5px 25px gray",
+            width: "170px",
+          }}
+        >
+          <Text color={"white"} as={"span"}>
+            Linkden{" "}
+          </Text>
+        </Button>{" "}
+        <Button
+          href="https://github.com/AdminForIinRange"
+          target="_blank"
+          leftIcon={<FaGithub color="WHITE" size={"20px"} />}
+          bgColor={"gray"}
+          variant={"solid"}
+          w={"100px"}
+          transition="transform, 0.3s ease-in-out"
+          _hover={{
+            transform: "scale(1.03)",
+            boxShadow: "0 5px 25px gray",
+            width: "170px",
+          }}
+        >
+          <Text color={"white"}>Github</Text>
+        </Button>
       </ButtonGroup>
     </>
   );
