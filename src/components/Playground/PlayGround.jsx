26b5b05bcc51c0981bc0204ca2852b7175e79f88
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-08 12:37:15 +0930

    jgdlj

--- src/components/Playground/PlayGround.jsx
+++ src/components/Playground/PlayGround.jsx
@@ -20,9 +20,9 @@ const PlayGround = () => {
     "Accelerate UI Development   in React using Amazing presets."
   );
 
-  const [Width, setWidth] = useState(620);
+  const [Width, setWidth] = useState("620px");
 
-  const [Height, setHeight] = useState(300);
+  const [Height, setHeight] = useState("300px");
 
   const [TextAlign, setTextAlign] = useState("start");
   const [Textjustify, setjustify] = useState("left");
@@ -32,27 +32,31 @@ const PlayGround = () => {
 
   const [BorderColor, setBorderColor] = useState("#B9B9B9");
   const [BorderRadius, setBorderRadius] = useState("10px");
-
+  const [BorderSize, setBorderSize] = useState("10px");
   const [TextColor, setTextColor] = useState("black");
 
   const [Animation, setAnimation] = useState("");
 
   return (
-    <HStack p={10}>
+    <HStack mt={isMobile ? "20px" : "80px"} justify="space-between" w="100%" h={"100%"} px={isMobile ? "10px" : "100px"}   >
       <VStack justify="center" w="100%" h="100%" align="center">
-        <Box w="100%" h={"100%"} bg="purple.200" rounded="xl" p={10}>
-          <Box
-            w="100%"
-            h={"100%"}
-            bg={BackgroundColor}
-            rounded={BorderRadius}
-            boxShadow={` 0px 0px ${BoxShadow} gray`}
-            borderColor={BorderColor}
-            p={4}
-          ></Box>
+        <Box w={"100%"} h={"100%"} bg="white" rounded="xl" py={12}  border={"1.8px solid #B5B5B5"}>
+          <VStack justify={"center"} w="100%" h={"100%"} align={"center"}>
+            {" "}
+            <Box
+              w={Width}
+              h={Height}
+              bg={BackgroundColor}
+              rounded={BorderRadius}
+              boxShadow={` 0px 0px ${BoxShadow} gray`}
+              border={`1.8px solid ${BorderColor}`}
+              borderColor={BorderColor}
+         
+            ></Box>
+          </VStack>
         </Box>
       </VStack>
-      <VStack justify="center" w="30%" h="100%" align="center">
+      <VStack justify="center" w="40%" h="100%" align="center">
         <Box w="100%" h={"100%"} bg="purple.200" rounded="xl" p={5}></Box>
       </VStack>
     </HStack>
