4f911b381a63f25f8c4bb61c3349ce262ad542bb
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-08 14:30:54 +0930

    ctreate a control

--- src/components/Playground/PlayGround.jsx
+++ src/components/Playground/PlayGround.jsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import {
   HStack,
   Text,
+  FormLabel,
+  Input,
   Box,
   Avatar,
   AvatarBadge,
@@ -17,30 +19,48 @@ const PlayGround = () => {
   const [isMobile] = useMediaQuery("(max-width: 736px)");
 
   const [heading, setHeading] = useState(
-    "Accelerate UI Development   in React using Amazing presets."
+    "Higher Education Should be free like in Europe"
   );
 
+  const [SubHeading, setSubHeading] = useState(
+    "Education is a human right, to exploit for monetary gain is immoral "
+  );
   const [Width, setWidth] = useState("620px");
 
   const [Height, setHeight] = useState("300px");
 
-  const [TextAlign, setTextAlign] = useState("start");
-  const [Textjustify, setjustify] = useState("left");
+  const [Align, setAlign] = useState("start");
+  const [Justify, setJustify] = useState("left");
 
   const [BackgroundColor, setBackgroundColor] = useState("#FFFFFF");
   const [BoxShadow, setBoxShadow] = useState("10px");
 
   const [BorderColor, setBorderColor] = useState("#B9B9B9");
   const [BorderRadius, setBorderRadius] = useState("10px");
-  const [BorderSize, setBorderSize] = useState("10px");
+  const [BorderSize, setBorderSize] = useState("1.8px");
   const [TextColor, setTextColor] = useState("black");
 
   const [Animation, setAnimation] = useState("");
 
   return (
-    <HStack mt={isMobile ? "20px" : "80px"} justify="space-between" w="100%" h={"100%"} px={isMobile ? "10px" : "100px"}   >
+    <HStack
+      transition="transform, 0.3s ease-in-out"
+      mt={isMobile ? "20px" : "80px"}
+      align={"start"}
+      w="100%"
+      h={"100%"}
+      px={isMobile ? "10px" : "100px"}
+    >
       <VStack justify="center" w="100%" h="100%" align="center">
-        <Box w={"100%"} h={"100%"} bg="white" rounded="xl" py={12}  border={"1.8px solid #B5B5B5"}>
+        <Box
+          transition="transform, 0.3s ease-in-out"
+          w={"100%"}
+          h={"100%"}
+          bg="white"
+          rounded="xl"
+          py={12}
+          border={`${BorderSize} solid ${BorderColor}`}
+        >
           <VStack justify={"center"} w="100%" h={"100%"} align={"center"}>
             {" "}
             <Box
@@ -51,13 +71,59 @@ const PlayGround = () => {
               boxShadow={` 0px 0px ${BoxShadow} gray`}
               border={`1.8px solid ${BorderColor}`}
               borderColor={BorderColor}
-         
-            ></Box>
+              transition="transform, 0.3s ease-in-out"
+              p={5}
+              px={6}
+            >
+              <Text
+                transition="transform, 0.3s ease-in-out"
+                fontSize={"30"}
+                fontWeight={"600"}
+                Align={Align}
+                justifyContent={Justify}
+              >
+                {heading}
+              </Text>
+              <Text
+                transition="transform, 0.3s ease-in-out"
+                fontSize={"16"}
+                fontWeight={"300"}
+                Align={Align}
+                justifyContent={Justify}
+              >
+                {SubHeading}
+              </Text>
+            </Box>
           </VStack>
         </Box>
       </VStack>
       <VStack justify="center" w="40%" h="100%" align="center">
-        <Box w="100%" h={"100%"} bg="purple.200" rounded="xl" p={5}></Box>
+        <Box
+          w="100%"
+          h={"100%"}
+          bg="white"
+          rounded="xl"
+          p={5}
+          border={`1.8px solid #B9B9B9`}
+        >
+          <FormLabel htmlFor="Heading"> Heading</FormLabel>
+          <Input
+            id="Heading"
+            type="text"
+            w={"100%"}
+            onChange={(e) => setHeading(e.target.value)}
+            value={heading}
+          />
+
+          <FormLabel htmlFor="SubHeading">SubHeading</FormLabel>
+          <Input
+            id="SubHeading"
+            type="text"
+            w={"100%"}
+            onChange={(e) => setSubHeading(e.target.value)}
+            value={SubHeading}
+          />
+        </Box>
       </VStack>
     </HStack>
   );
